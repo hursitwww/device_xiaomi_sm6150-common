ea84cc2128223ce24417715e7f939c9c10a45f2b
hursitwww <hursitwww@outlook.com>
2025-12-27 11:35:21 +0000

    Converting soong_config_set to soong_config_set_bool for build error

--- sm6150.mk
+++ sm6150.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
-$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
